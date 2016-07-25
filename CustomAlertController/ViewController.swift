8e2c1e83fc7771e0aa4f730ea3d4daf22ee97367
yusufkildan <kildanyusuf@gmail.com>
2016-07-25 16:34:49 +0300

    Initial Commit

---
+++ CustomAlertController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomAlertController
+//
+//  Created by yusuf_kildan on 25/07/2016.
+//  Copyright © 2016 yusuf_kildan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
